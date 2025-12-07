3f78571be569773168daab11ad37a2adedf68b99
unknown <rajnigupta2002@gmail.com>
2025-12-07 23:26:32 +0530

    VM creation

---
+++ main.tf
@@ -0,0 +1,62 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "htechrgs"
+  location = "Central India"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "htechrgs-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "htechrgs-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "htechrg-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "htechrgs-ip"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "virtual_machine" {
+  name                = "htechrgs-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+admin_ssh_key {
+  username   = "adminuser"
+  public_key = file("D:/htech_prac/.ssh/id_rsa.pub")
+}
+
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
